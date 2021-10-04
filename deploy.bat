c2f5597a90a19a1607c942a74f854c645e1b93fc
ElishaMayer <elisja.mayer@gmail.com>
2021-10-04 10:11:46 +0300

    fix deploy

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,9 @@
-#call npm run build
+call npm run build
 call npm version patch
 call RMDIR "hosting/public" /S /Q
 call MKDIR "hosting/public"
-call XCOPY "build" "hosting/public"
+call XCOPY /S "build" "hosting/public"
 call cd "hosting"
-#call firebase deploy
+call firebase deploy
+call cd ..
+call git push
